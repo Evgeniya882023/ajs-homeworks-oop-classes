ee9193bd1c11cb9340b107bfcc9a1f6012b02b29
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-10-16 21:30:31 +0300

    separated tests

--- src/character.js
+++ src/character.js
@@ -1,9 +1,5 @@
 export default class Character {
   constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
     const typeClass = [
       'Bowman',
       'Daemon',
@@ -26,5 +22,9 @@ export default class Character {
         'Выберите один из доступных типов классов: Bowman, Daemon, Magician, Swordsman, Undead, Zombie',
       );
     }
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
   }
 }
